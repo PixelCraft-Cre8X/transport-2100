267a9bb872623c0bb91f6a716a85ab792f192925
Buhuni Levanya <buhunilevanya@gmail.com>
2026-09-20 10:30:05 +0530

    Journey AI demo updatee

--- cre8x-transport-2100/src/utils/voiceSession.js
+++ cre8x-transport-2100/src/utils/voiceSession.js
@@ -1,6 +1,7 @@
 import { microphoneFailure, microphoneNotice } from "./microphone.js";
 
-export const voiceGreeting = " Hi, I’m Journey AI. Where would you like to go?";
+export const voiceGreeting =
+  "Hi, I’m Journey AI. Where would you like to go? You can tell me a destination or ask for the fastest, cheapest, or most comfortable journey.";
 export const recognitionUnavailable =
   "Voice recognition is not supported in this browser. You can still type your request.";
 
