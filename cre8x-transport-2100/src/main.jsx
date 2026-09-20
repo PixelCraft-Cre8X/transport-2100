70bcdd86acc24ee261a0a630fa4e8fd9768c9e54
K Dineth dovindu <kdinethdovindu@gmail.com>
2026-09-20 13:14:12 +0530

    Title name and logo update

--- cre8x-transport-2100/src/main.jsx
+++ cre8x-transport-2100/src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
 import App from "./App.jsx";
+
+document.title = "moveone";
+const favicon = document.querySelector('link[rel="icon"]') || document.createElement("link");
+favicon.rel = "icon";
+favicon.type = "image/png";
+favicon.href = new URL("./assets/logo.png", import.meta.url).href;
+document.head.appendChild(favicon);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
